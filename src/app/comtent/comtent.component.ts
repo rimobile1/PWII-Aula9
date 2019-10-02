45c484516ce82d8971bf8bd6d0bfd63f0073a159
rimobile1 <icaro.mo1234@gmail.com>
2019-10-02 20:38:44 -0300

    Aplicação header,content,footer

---
+++ src/app/comtent/comtent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-comtent',
+  templateUrl: './comtent.component.html',
+  styleUrls: ['./comtent.component.css']
+})
+export class ComtentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
