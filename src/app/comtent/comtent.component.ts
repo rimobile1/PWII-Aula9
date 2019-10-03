93e2e89c7df3affa365da0af5f73767f24aedfe1
rimobile1 <icaro.mo1234@gmail.com>
2019-10-02 22:23:58 -0300

    animaçao

--- src/app/comtent/comtent.component.ts
+++ src/app/comtent/comtent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'has-comtent',
@@ -10,6 +11,20 @@ export class ComtentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['ETEC Banking', 'Peça a portabilidade hoje mesmo', 'Inovavor, digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+
+    const typed = new Typed('.typing-element', options);
   }
 
 }
